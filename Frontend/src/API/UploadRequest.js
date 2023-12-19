fce22039c9a8505a1e6452fe007f167a36254709
Диана <Diana@MacBook-Pro-Aleksej.local>
2023-12-19 09:24:08 +0300

    fixed env

--- Frontend/src/API/UploadRequest.js
+++ Frontend/src/API/UploadRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API = axios.create({ baseURL: 'http://localhost:4000' });
+const API = axios.create({ baseURL: process.env.REACT_APP_API });
 
 export const uploadImage = (data) => API.post('/upload/', data);
 
